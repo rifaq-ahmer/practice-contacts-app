2055a6489b68175b77f775d981cf75c5c7f61a6e
rifaq-ahmer <rifaq.ahmer@gmail.com>
2021-05-07 03:04:33 +0530

    completed-form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [dropdown, setDropdown] = useState("");
+	const [radio, setRadio] = useState("");
+
 	return (
-		<div>
-			<h1>in Contact App</h1>
-		</div>
+		<>
+			<div>
+				<label htmlFor="name">Full Name</label>
+				<input
+					type="text"
+					name="name"
+					placeholder="Enter your full Name "
+					value={name}
+					onChange={(e) => {
+						setName(e.target.value);
+					}}
+					required
+				/>
+				<label htmlFor="email">E-mail</label>
+				<input
+					type="email"
+					name="email"
+					placeholder="Enter your email-Id "
+					value={email}
+					onChange={(e) => {
+						setEmail(e.target.value);
+					}}
+					required
+				/>
+
+				<select
+					name="dropdown"
+					id="dropdown"
+					value={dropdown}
+					onChange={(e) => {
+						setDropdown(e.target.value);
+					}}
+				>
+					<option value="Front-End">Front-End</option>
+					<option value="Back-End">Back-End</option>
+					<option value="Full-Stack">Full-Stack</option>
+				</select>
+
+				<label htmlFor="Male">Male</label>
+				<input
+					name="Male"
+					type="radio"
+					checked={radio === "Male"}
+					value="Male"
+					onChange={(e) => {
+						setRadio(e.target.value);
+					}}
+				/>
+
+				<label htmlFor="Female">female</label>
+				<input
+					name="Female"
+					type="radio"
+					checked={radio === "Female"}
+					value="Female"
+					onChange={(e) => {
+						setRadio(e.target.value);
+					}}
+				/>
+			</div>
+
+			<button>Save</button>
+		</>
 	);
 };
 
